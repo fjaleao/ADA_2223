a390536cd786eaa441d08b5df4f19f61fb1112c2
GuilhermeSantana <gf.santana@campus.fct.unl.pt>
2023-03-06 11:57:56 +0000

    max

---
+++ maxNumbers/src/MaxNumbers.java
@@ -0,0 +1,10 @@
+public class MaxNumbers {
+    private int maxNumber;
+    private String line;
+
+    public MaxNumbers(String line){
+        this.line = line;
+        maxNumber = Math.
+    }
+
+}
