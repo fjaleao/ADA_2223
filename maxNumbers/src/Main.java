7c47ceef7685ac40e9314e13150683fdbba0382e
f-leao <fjaleao@gmail.com>
2023-03-06 19:08:30 +0000

    done
    Co-authored-by: Guilherme Santana <GuiSantana08@users.noreply.github.com>

--- maxNumbers/src/Main.java
+++ maxNumbers/src/Main.java
@@ -1,14 +1,46 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Reader;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         // System.out.println("Hello world!");
 
         int maxValue;
 
-        Reader rd = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+
+        int nChildren = Integer.parseInt(rd.readLine());
+
+        // debug nChildren
+        System.out.println(nChildren + " children");
+
+        maxValue = processFindings(rd, nChildren);
+
+        System.out.println(maxValue);
+
+        rd.close();
 
 
+    }
+
+    private static int processFindings(BufferedReader rd, int nChildren) throws IOException {
+
+        int result = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nChildren; i++) {
+
+            String[] values = rd.readLine().split(" ");
+
+            int nSticks = Integer.parseInt(values[0]);
+
+            System.out.println("child number" + (i+1) + "picked up" + nSticks);
+
+            for (int j = 0; j < nSticks; j++) {
+                int current = Integer.parseInt(values[j+1]);
+                result = current >= result ? current : result;
+            }
+
+        }
+        return result;
     }
 }
