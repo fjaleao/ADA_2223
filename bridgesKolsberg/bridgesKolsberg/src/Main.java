69d04418114eda14bea441b43fb1d36ff586783e
f-leao <fjaleao@gmail.com>
2023-03-28 17:31:33 +0100

    Revert "Merge branch 'main' of https://github.com/fjaleao/ADA_2223"
    This reverts commit 197eee1f1147317d34c59679431d8c4edc7d978a, reversing
    changes made to ac7106ed375c76a6e2822985dd84213bb3d47b87.

--- bridgesKolsberg/bridgesKolsberg/src/Main.java
+++ bridgesKolsberg/bridgesKolsberg/src/Main.java
@@ -1,13 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Main {
-
-
-
-
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
     }
 }
