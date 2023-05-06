942c0b875fbc81444e44ca215626aacbf79ea3ec
f-leao <fjaleao@gmail.com>
2023-05-06 19:47:38 +0100

    initial commit assignment 2

--- wasItADream/wasItADream/src/Main.java
+++ wasItADream/wasItADream/src/Main.java
@@ -1,5 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    private static final char SPACE = ' ';
+    private static final char CARRIAGE_RETURN = '\n';
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+        int rows = getNextInt(rd, SPACE);
+        int columns = getNextInt(rd, SPACE);
+        int nTests = getNextInt(rd, CARRIAGE_RETURN);
+        char[][] board = getBoard(rd, columns, rows);
+
+        Tester t = new Tester(board);
+
+        for (int i = 0; i < nTests; i++) {
+
+            int res = t.resolve(getNextInt(rd, SPACE), getNextInt(rd, CARRIAGE_RETURN));
+
+            System.out.println(res < 0 ? "stuck" : res);
+
+        }
+
+    }
+
+    private static int getNextInt(BufferedReader rd, char delimiter) throws IOException {
+        String rows = "";
+        char b = (char) rd.read();
+        while (b != delimiter) {
+            rows += b;
+            b = (char) rd.read();
+        }
+        return Integer.parseInt(rows);
+    }
+
+    private static char[][] getBoard(BufferedReader rd, int colums, int rows) throws IOException {
+        char[][] board = new char[rows][colums];
+
+        for (int i = 0; i < rows; i++) {
+            board[i] = rd.readLine().toCharArray();
+        }
+
+        return board;
     }
 }
