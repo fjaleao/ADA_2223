c57175d31850dd5b88415928a374f8523bceda2a
Guilherme Santana <gf.santana@campus.fct.unl.pt>
2023-05-11 22:57:17 +0100

    erro

--- wasItADream/wasItADream/src/Main.java
+++ wasItADream/wasItADream/src/Main.java
@@ -8,24 +8,24 @@ public class Main {
     private static final int BONDARY = 2;
 
     private static final char SPACE = ' ';
-    private static final char CARRIAGE_RETURN = '\n';
+    private static final char CARRIAGE_RETURN = '\r';
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
-        try {
+        System.out.println("Apanahr linhas");
             int rows = getNextInt(rd, SPACE);
+        System.out.println("Apanahr colunas");
             int columns = getNextInt(rd, SPACE);
+                    System.out.println("Apanahr testes");
             int nTests = getNextInt(rd, CARRIAGE_RETURN);
+        System.out.println("Apanahr quadro");
             char[][] board = getBoard(rd, columns, rows);
+        System.out.println("Apanahr tester");
             Tester t = new Tester(board);
             for (int i = 0; i < nTests; i++) {
                 int res = t.resolve(getNextInt(rd, SPACE) -1, getNextInt(rd, CARRIAGE_RETURN) -1);
-                System.out.println(res < 0 ? "stuck" : res);
+                System.out.println(res < 0 ? "Stuck" : res);
             }
-        } catch (Exception e) {
-
-        }
 
     }
 
@@ -36,7 +36,10 @@ public class Main {
             rows += b;
             b = (char) rd.read();
         }
-        return Integer.parseInt(rows);
+        System.out.println("String " + rows);
+        int i = Integer.parseInt(rows);
+        System.out.println("number " + i);
+        return i;
     }
 
     private static char[][] getBoard(BufferedReader rd, int colums, int rows) throws IOException {
--- wasItADream/wasItADream/src/Tester.java
+++ wasItADream/wasItADream/src/Tester.java
@@ -23,71 +23,79 @@ public class Tester {
            found[cX][cY] = true;
            if(board[cX][cY] == 'H')
                return c.getCount();
+           boolean newPath = false;
            //go up
            int up = cX;
-           if(up-1 >= 0 && !found[up-1][cY]){
-               while(up-1 >= 0 && board[up-1][cY] != 'O' && board[up-1][cY] != 'H'){
+           while(up >= 0 && board[up][cY] != 'O' && board[up][cY] != 'H'){
+               if(!found[up][cY]) {
                    found[up][cY] = true;
-                   up--;
+                   newPath = true;
                }
+               up--;
            }
-           if(up-1 >= 0) {
-               char upChar = board[up - 1][cY];
+           if(up >= 0) {
+               char upChar = board[up][cY];
                if (upChar == 'H')
                    return c.getCount() + 1;
-               else if (board[up - 1][cY] == 'O' && !found[up][cY]) {
-                   waiting.add(new Coordinate(up, cY, c.getCount() + 1));
+               else if (board[up][cY] == 'O' && newPath) {
+                   waiting.add(new Coordinate(up+1, cY, c.getCount() + 1));
                }
            }
 
            //go down
+           newPath = false;
            int down = cX;
-           if(down+1 < board.length && !found[down+1][cY]){
-               while(down+1 < board.length && board[down+1][cY] != 'O' && board[down+1][cY] != 'H'){
+           while(down < board.length && board[down][cY] != 'O' && board[down][cY] != 'H'){
+               if(!found[down][cY]) {
                    found[down][cY] = true;
-                   down++;
+                   newPath = true;
                }
+               down++;
            }
            if(down+1 < board.length) {
-               char downChar = board[down+1][cY];
+               char downChar = board[down][cY];
                if (downChar == 'H')
                    return c.getCount() + 1;
-               else if (board[down+1][cY] == 'O' && !found[down][cY]) {
-                   waiting.add(new Coordinate(down, cY, c.getCount() + 1));
+               else if (board[down][cY] == 'O' && newPath) {
+                   waiting.add(new Coordinate(down-1, cY, c.getCount() + 1));
                }
            }
 
            //go left
+           newPath = false;
            int left = cY;
-           if(left-1 >= 0 && !found[cX][left-1]){
-                while(left-1 >= 0 && board[cX][left-1] != 'O' && board[cX][left-1] != 'H'){
-                     found[cX][left] = true;
-                     left--;
-                }
+           while(left >= 0 && board[cX][left] != 'O' && board[cX][left] != 'H'){
+               if(!found[cX][left]) {
+                   found[cX][left] = true;
+                   newPath = true;
+               }
+               left--;
            }
-           if(left-1 >= 0) {
-               char leftChar = board[cX][left-1];
+           if(left >= 0) {
+               char leftChar = board[cX][left];
                if (leftChar == 'H')
                    return c.getCount() + 1;
-               else if (board[cX][left-1] == 'O' && !found[cX][left]) {
-                   waiting.add(new Coordinate(cX, left, c.getCount() + 1));
+               else if (board[cX][left] == 'O' && newPath) {
+                   waiting.add(new Coordinate(cX, left+1, c.getCount() + 1));
                }
            }
 
            //go right
+           newPath = false;
            int right = cY;
-           if(right+1 < board[cX].length && !found[cX][right+1]){
-                while(right+1 < board[cX].length && board[cX][right+1] != 'O' && board[cX][right+1] != 'H'){
-                     found[cX][right] = true;
-                     right++;
-                }
+           while(right < board[cX].length && board[cX][right] != 'O' && board[cX][right] != 'H') {
+               if (!found[cX][right]) {
+                   found[cX][right] = true;
+                   newPath = true;
+               }
+               right++;
            }
-           if(right+1 < board[cX].length) {
-               char rightChar = board[cX][right+1];
+           if(right < board[cX].length) {
+               char rightChar = board[cX][right];
                if (rightChar == 'H')
                    return c.getCount() + 1;
-               else if (board[cX][right+1] == 'O' && !found[cX][right]) {
-                   waiting.add(new Coordinate(cX, right, c.getCount() + 1));
+               else if (board[cX][right] == 'O' && newPath) {
+                   waiting.add(new Coordinate(cX, right-1, c.getCount() + 1));
                }
            }
         }while(!waiting.isEmpty());
